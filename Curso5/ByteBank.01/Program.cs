50d8f68645ebd769033d05b1c87f03f075bf7a23
Luan Contarin da Silva <luan.silvacontarin@gmail.com>
2022-04-15 16:42:28 -0300

    conserto de diretórios e projeto utilizando Humanizer

---
+++ Curso5/ByteBank.01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Humanizer;
+
+namespace ByteBank._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFim = new DateTime(2022, 4, 20);
+            DateTime dataInicio = DateTime.Now;
+
+            TimeSpan diferenca = dataFim - dataInicio;
+
+            Console.WriteLine("Data início: " + dataInicio);
+            Console.WriteLine("Data fim: " + dataFim);
+
+            Console.WriteLine("Diferença das datas: " + TimeSpanHumanizeExtensions.Humanize(diferenca));
+
+            Console.ReadLine();
+        }
+    }
+}
--- Curso5/ByteBank.01/Properties/AssemblyInfo.cs
+++ Curso5/ByteBank.01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProgramaTeste")]
+[assembly: AssemblyTitle("ByteBank.01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProgramaTeste")]
+[assembly: AssemblyProduct("ByteBank.01")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e230e0aa-e743-42a8-b09a-b3008d2a23d1")]
+[assembly: Guid("0b6b903a-8f7e-437b-ace1-9d68d7c2d34a")]
 
 // Version information for an assembly consists of the following four values:
 //
