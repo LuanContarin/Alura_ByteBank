03e0e6e3d88ebe536b081eacde46f9670594a7e1
Luan Contarin da Silva <luan.silvacontarin@gmail.com>
2022-04-21 21:47:05 -0300

    Implementação da Classe Lista genérica e testes

---
+++ ByteBank.03/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestaBusca();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void TestaBusca()
+        {
+            Lista<ContaCorrente> listaDeContas = new Lista<ContaCorrente>();
+
+            var conta1 = new ContaCorrente(105, 245829, new Cliente(86797867953, "Luan C."));
+            var conta2 = new ContaCorrente(140, 735736, new Cliente(58947594595, "Pedro T."));
+            var conta3 = new ContaCorrente(105, 483748, new Cliente(48628462842, "João A."));
+
+            listaDeContas.Adicionar(conta1, conta2, conta3);
+
+            Console.WriteLine("Todas as contas:");
+            EscreverContas(listaDeContas);
+
+            var buscarPorAgencia = listaDeContas.Buscar(x => x.Agencia == 105);
+            Console.WriteLine("Contas buscadas pela agência 105:");
+            EscreverContas(buscarPorAgencia);
+
+            var buscarPorTitular = listaDeContas.Buscar(x => x.Titular.Nome.Contains("A"));
+            Console.WriteLine("Contas que contêm \"A\" no nome do titular: (case sensitive)");
+            EscreverContas(buscarPorTitular);
+        }
+
+        static void TestaRemocao()
+        {
+            Lista<int> listaIdades = new Lista<int>(5);
+            listaIdades.Adicionar(4, 5, 6, 2, 9, 10, 2, 7, 10);
+
+            Console.WriteLine("Lista completa:");
+            Escreverlistas(listaIdades);
+
+            // Remove o valor no index 0.
+            listaIdades.Remover(index: 0);
+            Console.WriteLine("Removendo item no index 0...");
+
+            // Remove apenas a primeira ocorrência do valor "2".
+            listaIdades.Remover(valor: 2);
+            Console.WriteLine("Removendo a primeira ocorrência do valor 2...");
+
+            // Remove todas as ocorrências do valor "10".
+            listaIdades.Remover(valor: 10, true);
+            Console.WriteLine("Removendo todas as ocorrências do valor 10...");
+
+            Console.WriteLine();
+            Console.WriteLine("Apos remoção dos itens descritos:");
+            Escreverlistas(listaIdades);
+        }
+
+        static void Escreverlistas<T>(params Lista<T>[] listas)
+        {
+            foreach (Lista<T> lista in listas)
+            {
+                Console.WriteLine("\t------------------");
+                Console.WriteLine($"\tItens da lista:");
+
+                int i = 0;
+                foreach (T item in lista)
+                {
+                    Console.WriteLine($"\tItem[{i}] = {item}");
+                    i++;
+                }
+                Console.WriteLine($"\tTamanho total: {lista.Tamanho}");
+
+                Console.WriteLine("\t------------------");
+                Console.WriteLine();
+            }
+        }
+
+        static void EscreverContas(Lista<ContaCorrente> contas)
+        {
+            foreach (var conta in contas)
+            {
+                Console.WriteLine("\t-------------------");
+                Console.WriteLine($"\tAgencia: {conta.Agencia}");
+                Console.WriteLine($"\tNumero conta: {conta.Numero}");
+                Console.WriteLine($"\tTitular: {conta.Titular.Nome}");
+                Console.WriteLine("\t-------------------");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Curso5/ByteBank.Modelos/Lista.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace ByteBank.Modelos
+{
+    public class Lista<T>
+    {
+        private T[] _itens;
+        private int _proximoIndex;
+
+        // Indexador
+        public T this[int index] { get { return _itens[index]; } }
+        public int Tamanho { get { return _proximoIndex; } }
+
+        public Lista(int capacidadeInicial = 10)
+        {
+            _itens = new T[capacidadeInicial];
+            _proximoIndex = 0;
+        }
+
+        public void Adicionar(T item)
+        {
+            VerificarTamanhoLista(_proximoIndex + 1);
+
+            _itens[_proximoIndex] = item;
+            _proximoIndex++;
+        }
+
+        public void Adicionar(params T[] itens)
+        {
+            foreach (T item in itens)
+            {
+                Adicionar(item);
+            }
+        }
+
+        public void Remover(int index)
+        {
+            if (index < 0 || index >= _proximoIndex)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            for (int i = index; i < _proximoIndex; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximoIndex--;
+        }
+
+        public void Remover(T valor, bool todos = false)
+        {
+            for (int i = 0; i < _proximoIndex; i++)
+            {
+                T itemAtual = _itens[i];
+
+                if (itemAtual.Equals(valor))
+                {
+                    Remover(i);
+                    if (!todos) break;
+                }
+            }
+        }
+
+        public Lista<T> Buscar(Predicate<T> condicao)
+        {
+            var novaLista = new Lista<T>();
+
+            for (int i = 0; i < _proximoIndex; i++)
+            {
+                T item = _itens[i];
+
+                if (condicao(item))
+                    novaLista.Adicionar(item);
+            }
+
+            return novaLista;
+        }
+
+        private void VerificarTamanhoLista(int proximoIndex)
+        {
+            int tamanhoAtual = _itens.Length;
+
+            if (proximoIndex <= tamanhoAtual)
+                return;
+
+            Array.Resize(ref _itens, tamanhoAtual * 2);
+        }
+
+        #region Enumerator
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public class Enumerator
+        {
+            private Lista<T> _lista;
+            private int _index;
+            public T Current => _lista[_index];
+
+            public Enumerator(Lista<T> lista)
+            {
+                _lista = lista;
+                _index = -1;
+            }
+
+            public bool MoveNext()
+            {
+                _index++;
+                return _index < _lista.Tamanho;
+            }
+        }
+
+        #endregion
+    }
+}
